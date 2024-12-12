7b227a5d28745d69b1a0527e35fc9e7a67fa1f52
sergi <sergiarabidze15@gmail.com>
2024-12-12 21:28:20 +0400

    task completed

--- app/src/main/java/com/example/android_bootcamp/MainActivity.kt
+++ app/src/main/java/com/example/android_bootcamp/MainActivity.kt
@@ -1,20 +1,32 @@
 package com.example.android_bootcamp
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.example.android_bootcamp.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.placeTextId.text = getString(R.string.place)
+        binding.priceStringId.text = getString(R.string.price_string)
+        binding.continentId.text = getString(R.string.continent)
+        binding.priceInNumberId.text = getString(R.string.price)
+        binding.currencyId.text = getString(R.string.currency)
+        binding.overviewId.text = getString(R.string.overview)
+        binding.detailsId.text = getString(R.string.details)
+        binding.clockId.text = getString(R.string.time)
+        binding.cloudId.text = getString(R.string.temperature)
+        binding.scrollViewId.text = getString(R.string.text)
+        binding.sendId.text = getString(R.string.book)
+        binding.starId.text = getString(R.string.star)
+
+
     }
 }
