2354a12b7a71b09e469e8e8b928598597d587c01
sergi <sergiarabidze15@gmail.com>
2025-02-09 23:31:22 +0400

    task completed

--- app/src/main/java/com/example/android_bootcamp/remote/paging/UserPagingSource.kt
+++ app/src/main/java/com/example/android_bootcamp/remote/paging/UserPagingSource.kt
@@ -4,6 +4,8 @@ import androidx.paging.PagingSource
 import androidx.paging.PagingState
 import com.example.android_bootcamp.remote.api.ServiceApi
 import com.example.android_bootcamp.remote.api.serializable_classes.User
+import com.example.android_bootcamp.remote.httpRequest.ApiHelper
+import com.example.android_bootcamp.remote.httpRequest.Resource
 
 class UserPagingSource(
     private val serviceApi: ServiceApi
@@ -11,26 +13,23 @@ class UserPagingSource(
 
     override suspend fun load(params: LoadParams<Int>): LoadResult<Int, User> {
         val page = params.key ?: 1
-        return try {
-            val response = serviceApi.getUsers(page)
-            val users = response.body()?.data ?: emptyList()
 
-            if(response.isSuccessful) {
-                val endOfPaginationReached = response.body()!!.data.isEmpty()
+        val result = ApiHelper.handleHttpRequest {
+            serviceApi.getUsers(page)
+        }
+
+        return when (result) {
+            is Resource.Success -> {
+                val users = result.data.data
+                val endOfPaginationReached = users.isEmpty()
                 LoadResult.Page(
                     data = users,
                     prevKey = if (page == 1) null else page - 1,
                     nextKey = if (endOfPaginationReached) null else page + 1
                 )
             }
-            else{
-                LoadResult.Error(Exception("No Response"))
-            }
-        } catch (e: Exception) {
-            LoadResult.Error(e)
-        }
-        catch (httpException: Exception) {
-            LoadResult.Error(httpException)
+            is Resource.Error -> LoadResult.Error(Exception(result.message))
+            is Resource.Loading, is Resource.Idle -> LoadResult.Error(Exception("Unexpected state"))
         }
     }
 
