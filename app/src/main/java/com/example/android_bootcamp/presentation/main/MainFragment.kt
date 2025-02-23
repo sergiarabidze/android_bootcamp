cd8a7e15562c2138e4a9a7f7f12c1236f4f0f036
sergi <sergiarabidze15@gmail.com>
2025-02-23 21:14:12 +0400

    task completed

--- app/src/main/java/com/example/android_bootcamp/presentation/main/MainFragment.kt
+++ app/src/main/java/com/example/android_bootcamp/presentation/main/MainFragment.kt
@@ -5,6 +5,7 @@ import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.android_bootcamp.common.BaseFragment
 import com.example.android_bootcamp.databinding.FragmentMainFragmentBinding
 import com.example.android_bootcamp.helper.resource.Resource
@@ -26,12 +27,38 @@ class MainFragment : BaseFragment<FragmentMainFragmentBinding>(FragmentMainFragm
      }
 
      override fun setRecycler() {
-          super.setRecycler()
+          binding.appbarLayout.setExpanded(false, false)
           binding.storyRecycler.adapter = storyAdapter
-          binding.storyRecycler.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.HORIZONTAL,false)
+          binding.storyRecycler.layoutManager = LinearLayoutManager(
+               requireContext(),
+               LinearLayoutManager.HORIZONTAL,
+               false
+          )
+
           binding.postsRecycler.adapter = postAdapter
-          binding.postsRecycler.layoutManager = LinearLayoutManager(requireContext(),LinearLayoutManager.VERTICAL,false)
+          binding.postsRecycler.layoutManager = LinearLayoutManager(
+               requireContext(),
+               LinearLayoutManager.VERTICAL,
+               false
+          )
+
+          binding.postsRecycler.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+               override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                    super.onScrolled(recyclerView, dx, dy)
+                    val layoutManager = recyclerView.layoutManager as? LinearLayoutManager ?: return
+                    val firstCompletelyVisiblePosition = layoutManager.findFirstCompletelyVisibleItemPosition()
+
+                    if (firstCompletelyVisiblePosition == 0) {
+                         binding.appbarLayout.setExpanded(true, true)
+                    } else {
+                         binding.appbarLayout.setExpanded(false, true)
+                    }
+               }
+          })
+
      }
+
+
 
      private fun observeStory() {
           viewLifecycleOwner.lifecycleScope.launch {
